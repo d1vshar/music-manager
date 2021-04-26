add66680ad57b6da377c7a2cabe9943fb8836623
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2021-04-27 00:52:06 +0530

    Add music insert sql
    Signed-off-by: Divyanshu Sharma <anshu.bishops.2001@gmail.com>

--- backend/src/db/MusicOps.ts
+++ backend/src/db/MusicOps.ts
@@ -1,19 +1,33 @@
-export const getMusicById = (id: string) => {
-    
-}
+import { v4 as uuidv4 } from 'uuid';
+import connection from './Connection';
+import logger from '../../config/winston';
+import { Music } from './types';
 
-export const getAllMusic = () => {
+export const getMusicById = (id: string) => {};
 
-}
+export const getAllMusic = () => {};
 
-export const createMusic = (newMusic: Music) => {
+export const createMusic = async (newMusic: Music): Promise<Music> => {
+  newMusic.id = uuidv4();
 
-}
+  const conn = await connection;
 
-export const updateMusic = (updatedMusic: Music) => {
+  await conn.execute(
+    'INSERT INTO music (id, name, file_path, music_length, release_year) '
+      + 'VALUES (?, ?, ?, ?, ?)',
+    [
+      newMusic.id,
+      newMusic.name,
+      newMusic.file,
+      newMusic.music_length,
+      newMusic.release_year,
+    ],
+  );
 
-}
+  logger.debug(`Inserted music: ${JSON.stringify(newMusic)}`);
+  return newMusic;
+};
 
-export const deleteMusicById = (id: string) => {
+export const updateMusic = (updatedMusic: Music) => {};
 
-}
+export const deleteMusicById = (id: string) => {};
