045cf16a69876b70297a3ff80f402b99b0b70aed
Divyanshu Sharma <anshu.bishops.2001@gmail.com>
2021-04-27 00:53:02 +0530

    add insert route
    Signed-off-by: Divyanshu Sharma <anshu.bishops.2001@gmail.com>

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,22 @@
-import express from "express";
-import logger from "./config/winston";
-import morganMiddleware from "./config/morgan";
+import dotenv from 'dotenv';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import logger from './config/winston';
+import morganMiddleware from './config/morgan';
+import music from './src/routes/music';
+
+dotenv.config();
 
 const app = express();
 
 const PORT = 8000;
 
 app.use(morganMiddleware);
+app.use(bodyParser.json());
+
+app.use(music);
+
 app.listen(PORT, () => {
-  logger.info(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+  logger.info(`Server is running at http://localhost:${PORT}`);
 });
--- backend/src/routes/music.ts
+++ backend/src/routes/music.ts
@@ -1,30 +1,42 @@
-import express from "express";
-import logger from "../../config/winston";
-import { createMusic, deleteMusicById, getMusicById, updateMusic } from "../db/MusicOps";
+import express from 'express';
+import logger from '../../config/winston';
+import {
+  createMusic,
+  deleteMusicById,
+  getMusicById,
+  updateMusic,
+} from '../db/MusicOps';
+import { Music } from '../db/types';
 
 const router = express.Router();
 
-router.get("/music/:id", (req, res) => {
-    const id: string = req.params.id;
-    const music = getMusicById(id);
+router.get('/music/:id', (req, res) => {
+  const { id } = req.params;
+  const music = getMusicById(id);
 
-    const data = music === null ? {} : music;
-    res.send(data);
+  const data = music === null ? {} : music;
+  res.send(data);
 });
 
-router.post("/music/new", (req, res) => {
-    const newMusic: Music = req.body;
-    createMusic(newMusic);
+router.post('/music/new', (req, res) => {
+  const newMusic: Music = req.body;
+  createMusic(newMusic)
+    .then((r) => res.json(r))
+    .catch((err) => {
+      logger.error(`Failed insertion music: ${JSON.stringify(err)}`);
+    });
 });
 
-router.post("/music/update", (req, res) => {
-    const updatedMusic: Music = req.body;
+router.post('/music/update', (req) => {
+  const updatedMusic: Music = req.body;
 
-    updateMusic(updatedMusic);
-})
+  updateMusic(updatedMusic);
+});
 
-router.delete("/music/:id", (req, res) => {
-    const id: string = req.params.id;
+router.delete('/music/:id', (req) => {
+  const { id } = req.params;
 
-    deleteMusicById(id);
-})
+  deleteMusicById(id);
+});
+
+export default router;
